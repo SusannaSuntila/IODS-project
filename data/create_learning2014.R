50de01965de45e5ffe201d44f9e6255268731e43
unknown <susanna.suntila@helsinki.fi>
2022-11-09 13:38:38 +0200

    data folder created, data wrangling

---
+++ data/create_learning2014.R
@@ -0,0 +1,63 @@
+
+
+# Susanna Suntila
+# Data wrangling for assignment 2
+
+date()
+
+
+# read the data into memory
+lrn14 <- read.table("http://www.helsinki.fi/~kvehkala/JYTmooc/JYTOPKYS3-data.txt", sep="\t", header=TRUE)
+
+# Look at the dimensions of the data
+dim(lrn14)
+
+# There are 183 rows and 60 columns
+
+# Look at the structure of the data
+str(lrn14)
+
+# Most of the variables are integers, but the gender-variable is a character
+
+# scale the attitude column
+lrn14$attitude <- lrn14$Attitude / 10
+
+# access the dplyr library
+library(dplyr)
+
+deep_questions <- c("D03", "D11", "D19", "D27", "D07", "D14", "D22", "D30","D06",  "D15", "D23", "D31")
+lrn14$deep <- rowMeans(lrn14[, deep_questions])
+surface_questions <- c("SU02","SU10","SU18","SU26", "SU05","SU13","SU21","SU29","SU08","SU16","SU24","SU32")
+lrn14$surf <- rowMeans(lrn14[, surface_questions])
+strategic_questions <- c("ST01","ST09","ST17","ST25","ST04","ST12","ST20","ST28")
+lrn14$stra <- rowMeans(lrn14[, strategic_questions])
+
+# choose a handful of columns to keep
+keep_columns <- c("gender","Age","attitude", "deep", "stra", "surf", "Points")
+
+# select the 'keep_columns' to create a new dataset
+learning2014 <- select(lrn14, one_of(keep_columns))
+
+# see the structure of the new dataset
+str(learning2014)
+
+# print out the column names of the data
+colnames(learning2014)
+
+# change the name of the second column
+colnames(learning2014)[2] <- "age"
+
+# change the name of "Points" to "points"
+colnames(learning2014)[7] <- "points"
+
+# print out the new column names of the data
+colnames(learning2014)
+
+# select rows where points is greater than zero
+learning2014 <- filter(learning2014, points > 0)
+
+library(tidyverse)
+write_csv(learning2014, "learning2014.csv")
+
+
+
