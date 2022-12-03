5a914ae70a83c3c96f0e88faa49339e5a69f3e21
unknown <susanna.suntila@helsinki.fi>
2022-12-03 13:01:26 +0200

    finished data wrangling week 5

--- data/create_human.R
+++ data/create_human.R
@@ -1,4 +1,9 @@
 
+# Susanna Suntila
+# create data "human"
+
+### week 4
+
 # Read in the datasets
 library(tidyverse)
 
@@ -22,7 +27,7 @@ str(gii)
 
 summary(gii)
 
-##
+# rename the variables
 library(dplyr)
 hd <- rename(hd, "HDI.r" = "HDI Rank",
               "HDI" = "Human Development Index (HDI)",
@@ -68,5 +73,49 @@ write_csv(human, "human.csv")
 read.csv(file.path(".", "data", "human.csv"))
 
 
+#### week 5
+
+# read the human data for week 5
+human <- read.table("https://raw.githubusercontent.com/KimmoVehkalahti/Helsinki-Open-Data-Science/master/datasets/human1.txt", 
+                    sep =",", header = T)
+
+# look at the structure and dimensions of the human data
+str(human) # country variable AND the GNI variable are characters, other variables are numbers or integers.
+dim(human) # there are 195 observations and 19 variables
+# this dataset is from the United Nations development programme
+# it includes components of the Human Development Index (HDI) and Gender Inequality Index (GII)
+
+# mutate the data
+human$GNI <- gsub(",", "", human$GNI) %>% as.numeric
+str(human) # now the GNI variable is a number as well
+
+# exclude unneeded variables
+# columns to keep
+keep <- c("Country", "Edu2.FM", "Labo.FM", "Life.Exp", "Edu.Exp", "GNI", "Mat.Mor", "Ado.Birth", "Parli.F")
+# select the 'keep' columns
+human <- dplyr::select(human, one_of(keep))
+
+# remove all rows with missing values
+human <- filter(human, complete.cases(human))
+
+# remove the observations which relate to regions instead of countries
+# define the last indice we want to keep
+last <- nrow(human) - 7
+# choose everything until the last 7 observations
+human <- human[1:last, ]
+
+# define the row names of the data by the country names
+rownames(human) <- human$Country
+# remove the country name column from the data
+human <- dplyr::select(human, -Country)
+
+#
+dim(human) # there are 155 observations and 8 variables
+
+# save the new modified data with row names
+library(tidyverse)
+write.table(human, "human.txt", row.names = TRUE)
+
+read.table(file.path(".", "data", "human.txt"))
 
 
